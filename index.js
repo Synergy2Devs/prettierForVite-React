f2f336b231ebb9a0748e8f224cd30f54358f4617
mnibor <marcelorobin@gmail.com>
2024-07-13 20:59:32 -0300

    cambios realizados

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const { spawn } = require('child_process');
+const path = require('path');
+const os = require('os');
+
+const isWindows = os.platform() === 'win32';
+const scriptPath = path.join(__dirname, 'prettier-vite-react.sh');
+
+let child;
+
+if (isWindows) {
+  // En Windows, usamos bash explícitamente
+  child = spawn('bash', [scriptPath], { stdio: 'inherit', shell: true });
+} else {
+  // En Unix (Linux/macOS), ejecutamos el script directamente
+  child = spawn(scriptPath, { stdio: 'inherit', shell: true });
+}
+
+child.on('exit', (code) => {
+  process.exit(code);
+});
