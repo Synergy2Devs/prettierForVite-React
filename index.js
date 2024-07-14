30f61092139f872f033825dc5677129f030a41fd
mnibor <marcelorobin@gmail.com>
2024-07-13 21:03:41 -0300

    .

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
 
-const { spawn } = require('child_process');
-const path = require('path');
-const os = require('os');
+const { spawn } = require('child_process')
+const path = require('path')
+const os = require('os')
 
-const isWindows = os.platform() === 'win32';
-const scriptPath = path.join(__dirname, 'prettier-vite-react.sh');
+const isWindows = os.platform() === 'win32'
+const scriptPath = path.join(__dirname, 'prettier-vite-react.sh')
 
-let child;
+let child
 
 if (isWindows) {
-  // En Windows, usamos bash explícitamente
-  child = spawn('bash', [scriptPath], { stdio: 'inherit', shell: true });
+	// En Windows, usamos bash explícitamente y escapamos la ruta
+	const escapedPath = scriptPath.replace(/\\/g, '/')
+	child = spawn('bash', [escapedPath], { stdio: 'inherit', shell: true })
 } else {
-  // En Unix (Linux/macOS), ejecutamos el script directamente
-  child = spawn(scriptPath, { stdio: 'inherit', shell: true });
+	// En Unix (Linux/macOS), ejecutamos el script directamente
+	child = spawn(scriptPath, { stdio: 'inherit', shell: true })
 }
 
 child.on('exit', (code) => {
-  process.exit(code);
-});
+	process.exit(code)
+})
